9c211828276ee6518c73d8d36319515413153abd
Edson Carlos Garcia Junior6 <ejunior6@Lenovo.com>
2023-07-05 10:51:32 -0300

    created-ts-config

---
+++ node_modules/@media-recovery-ui/tokens/src/font-weights.ts
@@ -0,0 +1,10 @@
+export const fontWeights = {
+    thin:'100',
+    light:'300',
+    regular: '400',
+    medium: '500',
+    semiBold: '600',
+    bold: '700',
+    extraBold: '800',
+    black: '900',
+}
---
+++ package/react/dist/index.d.ts
@@ -0,0 +1,1 @@
+export { colors } from '@media-recovery-ui/tokens';
