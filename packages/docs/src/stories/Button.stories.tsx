2f529ea1dcbb6266db6aada6b487dadd8308cb1d
Edson Garcia <edsonjr-garcia@live.com>
2023-07-06 13:31:55 -0300

    storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,14 @@
+import type { StoryObj } from '@storybook/react'
+import { Button } from '@media-recovery-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  }
+}
+
+export const Primary: StoryObj = {}
+
+export const Secondary: StoryObj = {}
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from './styles';
+
+export const Button = styled('button',{
+    backgroundColor:'$ignite300',
+    fontFamily: '$default',
+    fontSize: '$xl',
+    padding:'$4 $8',
+    border: 0,
+    borderRadius: '$md',
+    fontWeight:'$bold',
+})
